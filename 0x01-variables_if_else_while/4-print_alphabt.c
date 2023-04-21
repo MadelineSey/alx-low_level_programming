cc500f5e08149bda32706ec2d7e45f19647db944
MadelineSey <seyrambadasu@gmail.com>
2023-04-21 09:09:26 -0700

    I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *description: print alphabets in lowercase
+  *Return: Always 0 (success)
+  */
+int main(void)
+{
+	int x = 97;
+
+	while (x <= 122)
+	{
+		if (x == 101 || x == 113)
+		{
+			x++;
+			continue;
+		}
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
