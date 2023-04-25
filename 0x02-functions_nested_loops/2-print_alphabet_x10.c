4f114ca99bd5c4f0822662e8bd9520825a922e4e
MadelineSey <seyrambadasu@gmail.com>
2023-04-25 10:07:19 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  *print_alphabet_x10 - print alphabet 10 times
+  *Return: Always 0
+  */
+void print_alphabet_x10(void)
+{
+	int x;
+	int y;
+
+	for (x = 1; x <= 10; x++)
+	{
+		for (y = 97; y < 123; y++)
+		{
+			_putchar(y);
+		}
+		_putchar('\n');
+	}
+}
