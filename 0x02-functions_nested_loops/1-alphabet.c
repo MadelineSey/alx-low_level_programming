078e42a9de5ede62c83c34a254e15d70be1221ff
MadelineSey <seyrambadasu@gmail.com>
2023-04-25 10:16:08 -0700

    I sometimes suffer from insomnia

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,7 @@
 #include "main.h"
 /**
-  *main - entry point
-  *description: print alphabets in lower case
-  *Return: Always 0 (success)
+  *print_alphabet - print lowercase alphabets
+  *Return: Always 0
   */
 void print_alphabet(void)
 {
