e36f4745a420778c82c13c70f596285746b74aaa
MadelineSey <seyrambadasu@gmail.com>
2023-04-27 11:02:17 -0700

    The shortest distance

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  *print_line - draw a straight line in the terminal
+  *@n: number of times _n should be printed
+  *Return: \n if n is 0 or less
+  */
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a <= n; a++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
