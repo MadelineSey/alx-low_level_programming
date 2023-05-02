5a581fc90ac30deaa2537259f3bf6b094579e07c
MadelineSey <seyrambadasu@gmail.com>
2023-05-02 12:26:10 -0700

    hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - entry point
+ *Description: generates random passwords for the program 101-crackme
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int pass[100];
+	int a, b, c;
+
+	b = 0;
+
+	srand(time(NULL));
+
+	for (a = 0; a < 100; a++)
+	{
+		pass[a] = rand() % 78;
+		b += (pass[a] + '0');
+		putchar(pass[a] + '0');
+		if ((2772 - b) - '0' < 78)
+		{
+			c = 2772 - b - '0';
+			b += c;
+			putchar(c + '0');
+			break;
+		}
+	}
+
+	return (0);
+}
+
