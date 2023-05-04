561a6e067d6d45b14bdc0e0b629969f445768caa
MadelineSey <seyrambadasu@gmail.com>
2023-05-04 13:44:51 -0700

    Expect the best

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+  *cap_string - capitalize all words of the string
+  *@str: the string
+  *Return: Always 0
+  */
+char *cap_string(char *str)
+{
+	int x;
+
+	x = 0;
+	while (str[x] != '\0')
+		if (str[0] > 96 && str[0] < 123)
+		{
+			str[0] = str[0] - 32;
+		}
+	if (str[x] == ' ' || str[x] == '\t' || str[x] == '\n' || str[x] == ','
+			|| str[x] == ';' || str[x] == '.' || str[x] == '!'
+			|| str[x] == '?' || str[x] == '"' || str[x] == '('
+			|| str[x] == ')' || str[x] == '{'
+			|| str[x] == '}')
+	{
+		if (str[x + 1] > 96 && str[x + 1] < 123)
+		{
+			str[x + 1] = str[x + 1] - 32;
+		}
+		x++;
+	}
+	return (str);
+}
