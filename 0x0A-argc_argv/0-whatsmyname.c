744d220fb3a6cbd800c3bcf950ec11955ac29381
MadelineSey <seyrambadasu@gmail.com>
2023-05-12 12:23:29 -0700

    It is not what they call you

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
 /**
   *main - prints its name, followed by a new line
   *@argc: argument counter
-  &@argv: argument vector
+  *@argv: argument vector
   *Return: print the new name, without having to compile it again
   */
 int main(int argc __attribute__((unused)), char *argv[])
