12895ca8426a9dc2e00e04d54c45577d01f49e71
MadelineSey <seyrambadasu@gmail.com>
2023-05-22 09:01:55 -0700

    File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-  *Main - prints the name of the file it was compiled from
+  *main - prints the name of the file it was compiled from
   *Return: Always 0(success)
   */
 int main(void)
